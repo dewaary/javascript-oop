799042e5a3f834db2c3fe13ec4498f2ea940c7ad
dewa.g.ari28 <dewa.g.ari28@gmail.com>
2021-07-21 20:25:30 +0700

    try finally method

--- error-handling.js
+++ error-handling.js
@@ -21,5 +21,26 @@ try {
     console.log("Kode Program selesai");
 }
 
+class Counter {
+    #counter = 0;
+
+    next(){
+        try {
+            return this.#counter;
+        } finally {
+            this.#counter++;
+        }
+    }
+}
+
+const counter = new Counter();
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
+console.log(counter.next());
+
+
 
 
