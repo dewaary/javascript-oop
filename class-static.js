ab807c494e920141663d1f3d94ac0f98f748e8bd
dewa.g.ari28 <dewa.g.ari28@gmail.com>
2021-07-21 20:00:42 +0700

    Class Static

---
+++ class-static.js
@@ -0,0 +1,13 @@
+class Configuration {
+    static name = "Belajar Javascript OOP";
+    static version = 1.0;
+    static author = "Dewa Ari";
+}
+
+const config = new Configuration();
+console.log(config);
+
+
+console.log(Configuration.name);
+console.log(Configuration.author);
+console.log(Configuration.version);
